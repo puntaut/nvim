8f6f9abd1681aad293e4897f3262ea00f036bd8d
Aut Suttitanamongkol <punt@punt.local>
2023-07-08 12:04:49 +0700

    08_nvim : Edit config oil.nvim and toggleterm.nvim

--- lua/core/plugins_config/oil.lua
+++ lua/core/plugins_config/oil.lua
@@ -1,3 +1,7 @@
-require("oil").setup()
+require("oil").setup{
+  view_options = {
+    show_hidden = true,
+  },
+}
 
 vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
--- lua/core/plugins_config/toggleterm.lua
+++ lua/core/plugins_config/toggleterm.lua
@@ -3,6 +3,14 @@ require("toggleterm").setup{
   direction = 'tab',
   on_open = function(term)
     vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-jj>", "exit<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-l>", "lazygit<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-k>", "clear<CR>", { noremap = true, silent = true })
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
   end,
   close_on_exit = true,
 }
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+end
