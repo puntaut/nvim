ff4053ab76ad25d615407806592e492aa23a8d69
Aut Suttitanamongkol <punt@punt.local>
2023-07-11 17:55:40 +0700

    09_nvim : New indentblanklinescheme

--- lua/core/plugins_config/indent_blankline.lua
+++ lua/core/plugins_config/indent_blankline.lua
@@ -1,13 +1,13 @@
 vim.g.indent_blankline_use_treesitter = true
 vim.opt.list = true
-vim.cmd [[highlight IndentBlanklineIndent0 guifg=#484848 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#5f5f5f gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#767676 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#8d8d8d gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#a4a4a4 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#bbbbbb gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#d2d2d2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent7 guifg=#e9e9e9 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent0 guifg=#4f4f4f gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#666666 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#7d7d7d gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#949494 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#ababab gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#c2c2c2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#d9d9d9 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent7 guifg=#f0f0f0 gui=nocombine]]
 require("indent_blankline").setup{
   char_highlight_list = {
     "IndentBlanklineIndent0",
