31fb72a4e7378915b0c5e5c5ee91429e94f7c256
Aut Suttitanamongkol <punt@punt.local>
2023-07-12 05:14:57 +0700

    12_nvim : Add toggleterm shortkey

--- lua/core/plugins_config/toggleterm.lua
+++ lua/core/plugins_config/toggleterm.lua
@@ -3,6 +3,8 @@ require("toggleterm").setup{
   direction = 'tab',
   on_open = function(term)
     vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-jj>", "exit<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-k>", "clear<CR>", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-l>", "lazygit<CR>", { noremap = true, silent = true })
   end,
   close_on_exit = true,
 }
