fd4548a61f689919619863680d6a619964417867
Aut Suttitanamongkol <punt@punt.local>
2023-08-09 06:11:01 +0700

    19.2_nvim : Telescope keymap Update!!

--- lua/core/plugins_config/telescope.lua
+++ lua/core/plugins_config/telescope.lua
@@ -53,9 +53,13 @@ telescope.setup {
 }
 telescope.load_extension('fzf')
 
+vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fp', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>ft', builtin.jumplist, {})
+vim.keymap.set('n', '<leader>fr', builtin.resume, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 vim.keymap.set('n', '<leader><CR>', builtin.lsp_definitions, {})
